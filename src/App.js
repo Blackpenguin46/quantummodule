7f4e34b149d6102c81e7734e2f3ce03502de25c9
Sam oakes <samoakes13@icloud.com>
2024-12-01 22:33:18 -0500

    new changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // Use HashRouter here
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // Use HashRouter for GitHub Pages
 import LandingPage from './components/LandingPage';
 import QuantumBasics from './modules/QuantumBasics';
 import QuantumRisks from './modules/QuantumRisks';
 import QuantumSafePractices from './modules/QuantumSafePractices';
 import BusinessAdoption from './modules/BusinessAdoption';
 import ClosingActivity from './modules/ClosingActivity';
-import Sidebar from './components/Sidebar'; // Import Sidebar component
-import './App.css'; // Import any global styles
+import Sidebar from './components/Sidebar';
+import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="app-container">
-        <Sidebar /> {/* Sidebar is now included */}
+        <Sidebar />
         <div className="main-content">
           <Routes>
-            <Route path="/" element={<LandingPage />} /> {/* Main page */}
+            <Route path="/" element={<LandingPage />} /> {/* Landing Page */}
             <Route path="/quantum-basics" element={<QuantumBasics />} />
             <Route path="/quantum-risks" element={<QuantumRisks />} />
             <Route path="/quantum-safe-practices" element={<QuantumSafePractices />} />
@@ -30,6 +30,7 @@ function App() {
 }
 
 export default App;
+
 
 
 
