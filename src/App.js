3d86c54894ad72ade6eb7b2923080df487359858
Sam oakes <samoakes13@icloud.com>
2024-12-01 19:38:32 -0500

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './components/LandingPage';
-import QuantumBasics from './modules/QuantumBasics';
-import QuantumRisks from './modules/QuantumRisks';
-import QuantumSafePractices from './modules/QuantumSafePractices';
-import BusinessAdoption from './modules/BusinessAdoption';
-import ClosingActivity from './modules/ClosingActivity';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Container, AppBar, Toolbar, Typography, Box } from '@mui/material';
+import Sidebar from './Sidebar';
+import LandingPage from './LandingPage';
+import QuantumBasics from './QuantumBasics';
+import QuantumRisks from './QuantumRisks';
+import QuantumSafePractices from './QuantumSafePractices';
+import BusinessAdoption from './BusinessAdoption';
+import ClosingActivity from './ClosingActivity';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/quantum-basics" element={<QuantumBasics />} />
-        <Route path="/quantum-risks" element={<QuantumRisks />} />
-        <Route path="/quantum-safe-practices" element={<QuantumSafePractices />} />
-        <Route path="/business-adoption" element={<BusinessAdoption />} />
-        <Route path="/closing-activity" element={<ClosingActivity />} />
-      </Routes>
+      <Box sx={{ display: 'flex' }}>
+        <Sidebar />
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            bgcolor: '#f4f4f9',
+            padding: '2rem',
+          }}
+        >
+          <AppBar position="sticky" sx={{ marginBottom: '1rem' }}>
+            <Toolbar>
+              <Typography variant="h6">Quantum Learning Module</Typography>
+            </Toolbar>
+          </AppBar>
+          <Container maxWidth="lg">
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/quantum-basics" element={<QuantumBasics />} />
+              <Route path="/quantum-risks" element={<QuantumRisks />} />
+              <Route path="/quantum-safe-practices" element={<QuantumSafePractices />} />
+              <Route path="/business-adoption" element={<BusinessAdoption />} />
+              <Route path="/closing-activity" element={<ClosingActivity />} />
+            </Routes>
+          </Container>
+        </Box>
+      </Box>
     </Router>
   );
 }
 
 export default App;
+
 
