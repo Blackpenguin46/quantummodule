17982b5423b1c50a649a0d9dade44eacc091f385
Sam oakes <samoakes13@icloud.com>
2024-12-01 19:24:58 -0500

    landingpage.js update

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,14 +1,23 @@
-// src/components/LandingPage.js
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const LandingPage = () => {
+function LandingPage() {
   return (
     <div>
       <h1>Welcome to the Quantum Learning Module!</h1>
       <p>Choose a module to get started.</p>
+
+      <ul>
+        <li><Link to="/quantum-basics">Quantum Basics</Link></li>
+        <li><Link to="/quantum-risks">Quantum Risks</Link></li>
+        <li><Link to="/quantum-safe-practices">Quantum Safe Practices</Link></li>
+        <li><Link to="/business-adoption">Business Adoption</Link></li>
+        <li><Link to="/closing-activity">Closing Activity</Link></li>
+      </ul>
     </div>
   );
-};
+}
 
 export default LandingPage;
+
 
